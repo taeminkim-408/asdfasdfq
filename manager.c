a77613f36564414ae9a023b36caf993f54eff91e
Kimtaemin <22000192@handong.ac.kr>
2023-04-23 23:23:30 +0900

    manager.c

--- manager.c
+++ manager.c
@@ -55,7 +55,21 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
+	fp=fopen("product.txt", "rt");
+        //파일 내용을 읽어와서 배열에 값 추가하기
+        if(fp==NULL){
+            printf("=> 파일 없음");
+        }
+        else{
+            while(!feof(fp)){
+                fscanf(fp, "%s", p[count].name);
+                if(feof(fp)) break;
+                fscanf(fp, "%d", &p[count].weight);
+                fscanf(fp, "%d", &p[count].price);
+            count++;
+        }
+        }
+        fclose(fp);
 
 
 
